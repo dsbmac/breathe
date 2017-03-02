c37e4101cac6dff48317d323de0cd8f8d2b3c2b2
dsb_mac <dsbmach@gmail.com>
2017-03-02 11:52:53 -0500

    fixed index android.js

--- index.android.js
+++ index.android.js
@@ -1,6 +1,13 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
 import {
   AppRegistry,
 } from 'react-native';
-import App from './app/index'
+import App from './app/index' 
 
 AppRegistry.registerComponent('AwesomeProject', () => App);

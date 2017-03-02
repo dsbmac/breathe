53db6f072d4928391044f3c47adb8d4fe6403637
dsb_mac <dsbmach@gmail.com>
2017-03-02 11:49:07 -0500

    add bananas

--- app/index.js
+++ app/index.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
+import Bananas from './Bananas';
 
 export default class App extends Component {
     render() {
         return (
-            <Text>Hello</Text>
+            // <Text>Hello</Text>
+            <Bananas/>
         );
     }
 }

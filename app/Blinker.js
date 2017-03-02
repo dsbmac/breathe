dab6c788c723fade90fcb5fb8f38a30f47ae7dd9
dsb_mac <dsbmach@gmail.com>
2017-03-02 12:41:32 -0500

    add styles

--- app/Blinker.js
+++ app/Blinker.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 import { Text } from 'react-native';
 import Blink from './Blink';
 
-export default class App extends Component {
+export default class Blinker extends Component {
     render() {
         return (
-            <Blink text="Suck me dry"/>
+            <Blink text="Suck me very dry"/>
         );
     }
 }
--- app/index.js
+++ app/index.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
-import Blinker from './Blinker';
+import LotsOfStyles from './LotsOfStyles';
 
 export default class App extends Component {
     render() {
         return (
-            <Blinker/>
+            <LotsOfStyles/>
         );
     }
 }
